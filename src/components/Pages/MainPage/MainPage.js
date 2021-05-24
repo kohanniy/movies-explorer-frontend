e706aaee2729a3ac991fa302c7615f12fc95099d
Вячеслав Коханный <xoxluwa1@yandex.com>
2021-05-24 22:11:34 +0500

    feat: make up desktop, tablet and mobile versions of the section AboutProject

--- src/components/Pages/MainPage/MainPage.js
+++ src/components/Pages/MainPage/MainPage.js
@@ -1,11 +1,13 @@
 import HeroBlock from '../../HeroBlock/HeroBlock';
 import NavTab from '../../NavTab/NavTab';
+import AboutProject from '../../AboutProject/AboutProject';
 
 const MainPage = () => {
   return (
     <>
       <HeroBlock />
       <NavTab />
+      <AboutProject />
     </>
   );
 }
