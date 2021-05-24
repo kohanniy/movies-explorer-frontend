3bd6cf013d5573b942752c41ba9abdbea9ba44ce
Вячеслав Коханный <xoxluwa1@yandex.com>
2021-05-24 15:05:37 +0500

    feat: make up desktop, tablet and mobile versions of the hero-block

---
+++ src/components/HeroBlock/HeroBlock.js
@@ -0,0 +1,13 @@
+import './HeroBlock.css';
+
+const HeroBlock = () => {
+  return (
+    <section className='hero'>
+      <h1 className='hero__title'>
+        Учебный проект студента факультета Веб-разработки.
+      </h1>
+    </section>
+  );
+}
+
+export default HeroBlock;
