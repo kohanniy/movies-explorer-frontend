1b29e1e96eddb72ba119c3d5555112fab04429cb
Вячеслав Коханный <xoxluwa1@yandex.com>
2021-06-04 16:43:35 +0500

    feat: set up app login

--- src/components/Pages/LoginPage.js
+++ src/components/Pages/LoginPage.js
@@ -2,13 +2,23 @@ import PageMainContent from '../PageMainContent/PageMainContent';
 import AuthSection from '../AuthSection/AuthSection';
 import { loginInputsData, loginSectionData } from '../../utils/constants';
 
-const LoginPage = ({ isAuthPage }) => {
+const LoginPage = (props) => {
+  const {
+    isAuthPage,
+    onLoginFormSubmit,
+    isLoading,
+    serverErrorMsg,
+  } = props;
+
   return (
     <PageMainContent>
       <AuthSection
         isAuthPage={isAuthPage}
         inputsData={loginInputsData}
         sectionData={loginSectionData}
+        isLoading={isLoading}
+        onFormSubmit={onLoginFormSubmit}
+        serverErrorMsg={serverErrorMsg}
       />
     </PageMainContent>
   );
